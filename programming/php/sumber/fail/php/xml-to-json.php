754a3cbabacb179b708a13579c846bcf5c6065b6
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-09 21:12:25 +0800

    tambah class cara tukar xml kepada json

---
+++ programming/php/sumber/fail/php/xml-to-json.php
@@ -0,0 +1,20 @@
+<?php
+# sumber rujukan
+# https://lostechies.com/seanbiefeld/2011/10/21/simple-xml-to-json-with-php
+class XmlToJson 
+{
+#####################################################################################
+#------------------------------------------------------------------------------------
+	public function parse($url) 
+	{
+		$fileContents= file_get_contents($url);
+		$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+		$fileContents = trim(str_replace('"', "'", $fileContents));
+		$simpleXml = simplexml_load_string($fileContents);
+		$json = json_encode($simpleXml);
+		return $json;
+	}
+#------------------------------------------------------------------------------------
+#####################################################################################
+}
+
